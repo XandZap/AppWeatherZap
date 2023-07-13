b7de7044020b1cc4c644250fcb2b666252acfaf9
XandZap <alexandreroberto.94@gmail.com>
2023-07-13 13:51:07 -0400

    ➕ add shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+
 export default function Home() {
   return (
-    <main className="text-gray-50">
-      <button>C° - F°</button>
-      <input type="text" placeholder="Search" />
+    <main className="text-gray-50 flex flex-col items-center gap-5">
+      <div className="flex ml-10 gap-1 self-baseline">
+        <span className="text-center">C° </span>
+        <Switch className="bg-green" />
+        <span className="text-center">F°</span>
+      </div>
+      <Input type="text" placeholder="Search" className="w-96 text-purple"/>
       <div>Weather Card Carrousel</div>
       <div>daily weather</div>
     </main>
